0bde035ae0d30cb468ff99d908b62dd653d70702
louis spitz <l_spitz@live.com>
2017-11-07 21:22:27 +0000

    A basic website

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,32 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary to make your level challenging.
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(450);
+        cannon.create.onTop(400);
+        cannon.create.onTop(350);
+        cannon.create.onTop(300);
+        cannon.create.onTop(500);
+        cannon.create.onTop(550);
+        cannon.create.onTop(250);
+        cannon.create.onTop(600);
+        cannon.create.onTop(650);
+        cannon.create.onTop(700);
+        cannon.create.onTop(750);
+        cannon.create.onTop(800);
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
